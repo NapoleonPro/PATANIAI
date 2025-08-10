1ed954806d437ccb55a1b37c00d8d2ee23569b0b
NapoleonPro <akpp020405@gmail.com>
2025-08-10 23:35:00 +0700

    awksdkacaqs

--- app.py
+++ app.py
@@ -50,4 +50,5 @@ with app.app_context():
 
 # --- Jalankan Aplikasi ---
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
