25e7fceeac0bc3634812c94b6f9cd589f7691a20
NapoleonPro <akpp020405@gmail.com>
2025-08-10 23:21:13 +0700

    bisa plis

--- routes/config.py
+++ routes/config.py
@@ -5,5 +5,8 @@ import os
 SECRET_KEY = os.getenv('SECRET_KEY', '7f41b1f8f1a9e0f8c9b01d985e64d1d201edb5bbf7c36b876f8bbd4e789d7a20')
 
 # --- Konfigurasi Database ---
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:Gemastikusk23@localhost:5432/agrolldb')
+database_url = os.getenv('DATABASE_URL', 'postgresql://postgres:Gemastikusk23@localhost:5432/agrolldb')
+if database_url.startswith("postgresql://"):
+    database_url = database_url.replace("postgresql://", "postgresql+psycopg://", 1)
+SQLALCHEMY_DATABASE_URI = database_url
 SQLALCHEMY_TRACK_MODIFICATIONS = False
